60c51ef4e781243c8708fbb7e75633fdf3f59658
YAMAGUCHI_WIN\takuy <takuya.yamaguchi0522@gmail.com>
2025-02-11 13:10:33 +0900

    "Update deployment configuration for Azure"

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BASE_URL: 'https://tech0-gen8-step4-pos-app-85.azurewebsites.net',
-    NEXT_PUBLIC_API_URL: 'https://tech0-gen8-step4-pos-app-86.azurewebsites.net/api'
+    NEXT_PUBLIC_API_URL: 'https://tech0-gen8-step4-pos-app-86.azurewebsites.net'
   },
-    // 以下を追加
-    output: 'standalone',
-    poweredByHeader: false
+  output: 'standalone',
+  experimental: {
+    outputStandalone: true
+  }
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
