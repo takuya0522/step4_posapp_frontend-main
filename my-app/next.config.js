8831415dd960a997d4bb3778cfaf3a61435cc6cb
YAMAGUCHI_WIN\takuy <takuya.yamaguchi0522@gmail.com>
2025-02-11 14:52:28 +0900

    同期

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -3,12 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_BASE_URL: 'https://tech0-gen8-step4-pos-app-85.azurewebsites.net',
-    NEXT_PUBLIC_API_URL: 'https://tech0-gen8-step4-pos-app-86.azurewebsites.net'
+    NEXT_PUBLIC_API_URL: 'https://tech0-gen8-step4-pos-app-86.azurewebsites.net/api'  // ここを確認
   },
   output: 'standalone',
   experimental: {
     outputStandalone: true
   }
 }
-
-module.exports = nextConfig
