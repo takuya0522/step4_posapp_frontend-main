f8b349f7e5f511766cf4f4ba76f1dabb6273539e
YAMAGUCHI_WIN\takuy <takuya.yamaguchi0522@gmail.com>
2025-02-12 21:20:00 +0900

    APIGetURL修正

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BASE_URL: 'https://tech0-gen8-step4-pos-app-85.azurewebsites.net',
-    NEXT_PUBLIC_API_URL: 'https://tech0-gen8-step4-pos-app-86.azurewebsites.net/api'  // ここを確認
+    NEXT_PUBLIC_BASE_URL: 'https://tech0-gen8-step4-pos-app-85.azurewebsites.net/',
+    NEXT_PUBLIC_API_URL: 'https://tech0-gen8-step4-pos-app-86.azurewebsites.net/'  // ここを確認
   },
   output: 'standalone',
   experimental: {
